200bef926463f88a828ff694f90481820250bb3d
klebermkardel <molinakardel@gmail.com>
2023-01-07 19:35:00 -0300

    Commit Final

--- Desafios Básicos JavaScript/Calculando Média Final de Notas/assets/JS/index.js
+++ Desafios Básicos JavaScript/Calculando Média Final de Notas/assets/JS/index.js
@@ -1,6 +1,4 @@
-/* Dado as quatro notas finais do aluno no ano, calcule a média final e 
-imprima no console as notas e a média final e verifique se o mesmo foi aprovado,
-ficou de recuperação ou foi reprovado.
+/* Dado as quatro notas finais do aluno no ano, calcule a média final e informe sua média e sua situação.
 
 01 - Se média for menor que 5, aluno reprovado. 
 02 - Se média for entre 5 e 7, aluno em recuperação.
@@ -10,27 +8,30 @@ ficou de recuperação ou foi reprovado.
 
 // Código:
 
-const n1 = 5
-const n2 = 7
-const n3 = 6
-const n4 = 8
+function calcular() {
+    var tn1 = document.getElementById('txtn1')
+    var tn2 = document.getElementById('txtn2')
+    var tn3 = document.getElementById('txtn3')
+    var tn4 = document.getElementById('txtn4')
+    var situacao = document.getElementById('situacao')
+    var n1 = Number(tn1.value)
+    var n2 = Number(tn2.value)
+    var n3 = Number(tn3.value)
+    var n4 = Number(tn4.value)
+    var media = (n1+n2+n3+n4)/4
 
-const media = (n1 + n2 + n3 + n4) / 4
+    if(media < 5) {
+        situacao.innerHTML = `Sua média foi de ${media} e infelizmente você foi reprovado`
+    } else if (media >= 5 && media < 7) {
+        situacao.innerHTML = `Sua média foi de ${media} e você ficou de recuperação`
+    } else {
+        situacao.innerHTML = `Sua média foi de ${media}! Parabéns, você foi aprovado!`
 
-console.log("Nota 1° Bimestre: " + n1)
-console.log("Nota 2° Bimestre: " + n2)
-console.log("Nota 3° Bimestre: " + n3)
-console.log("Nota 4° Bimestre: " + n4)
-
-console.log("Média final: " + media)
-
-if(media < 5) {
-    console.log("Infelizmente você foi reprovado");
-} else if (media >= 5 && media < 7) {
-    console.log("Você ficou de recuperação")
-} else {
-    console.log("Parabéns, você foi aprovado!")
+    }
+    
 }
+
+
 
 
 
